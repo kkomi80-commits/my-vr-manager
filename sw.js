ba2d8d97e57b193e2c3bf27fbd5543445d972944
kkomi80-commits <kkomi80@gmail.com>
2026-02-12 14:32:38 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,16 @@
-const CACHE_NAME = 'vr-manager-v1';
+const CACHE_NAME = 'vr-manager-v2';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json'
 ];
 
-// 설치 시 자원 캐싱
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// 네트워크 우선 전략 (가격 데이터 때문)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
